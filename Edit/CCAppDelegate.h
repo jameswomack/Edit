abbe611aa2103f65a689064b5a05ef79fa45855d
James Womack <jamesw@amcoonline.net>
2013-02-20 18:49:58 -0800

    Initial Commit

---
+++ Edit/CCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CCAppDelegate.h
+//  Edit
+//
+//  Created by James Womack on 2/20/13.
+//  Copyright (c) 2013 James Womack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CCViewController;
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CCViewController *viewController;
+
+@end
---
+++ Edit/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  Edit
+//
+//  Created by James Womack on 2/20/13.
+//  Copyright (c) 2013 James Womack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
